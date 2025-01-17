1ff423d8531edfbc4b47e40c7208a0352cada348
adrncrlvrgs <imadrianvargas@gmail.com>
2025-01-18 04:03:34 +0800

    fix overscroll

--- src/components/Pages/Page.js
+++ src/components/Pages/Page.js
@@ -3,13 +3,12 @@ import PageHeader from "./PageHeader";
 import PageMain from "./PageMain";
 import PageFooter from "./PageFooter";
 import PageSideBar from "./PageSideBar";
-import { useAuth } from "context/AuthContext";
 
 export default function Page(props) {
   const { children } = props;
-  
+
   return (
-    <div className="min-h-screen flex flex-col justify-center content-center">
+    <div className="min-h-screen flex flex-col">
       <PageHeader />
       <div className="flex flex-1">
         <PageSideBar />
--- src/components/Pages/PageMain.js
+++ src/components/Pages/PageMain.js
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function PageMain(props) {
   const { children } = props;
-  return <div className="flex-1 bg-slate-50">{children}</div>;
+  return <main className="flex-1 p-4 overflow-y-auto">{children}</main>;
 }
