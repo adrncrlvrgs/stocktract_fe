ba9fa431aafebd618db3f5c08f66793946b991e2
adrncrlvrgs <imadrianvargas@gmail.com>
2025-01-26 12:09:34 +0800

    fix ui

--- src/modules/sales/sales-report/view/SalesReport.js
+++ src/modules/sales/sales-report/view/SalesReport.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import useSalesReport from "../hooks/useSalesReport";
 import { Input } from "components/Input";
-import FormGroup from "components/Form/FormGroup";
+import FormGroup from "components/Form/FormGroup.js";
 import { Button } from "components/Button";
 import CardContainer from "components/Cards/CardContainer";
 
