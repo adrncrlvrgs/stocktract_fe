4c83ca57a964816bd52f080ffdea0d79b5d1563f
adrncrlvrgs <imadrianvargas@gmail.com>
2025-01-26 12:36:17 +0800

    fix ui

--- src/modules/activity-logs/view/LogsTable.js
+++ src/modules/activity-logs/view/LogsTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "components/Table";
+import { Table } from "components/Table/Table";
 import moment from "moment";
 
 const LogsTable = (props) => {
